1650a9a4d56ff00dcbd56d6dfaa46d4550732860
Tom Dobra <tom.dobra@btinternet.com>
2020-06-09 17:08:40 +0100

    pdftex only compiler using native promises

--- pdftexlight.js
+++ pdftexlight.js
@@ -11,8 +11,8 @@ class TeXLive {
     this.onlog = obj.onlog;
     addEventListener("unload", () => { this.terminate(); });
     //Public functions
-    this.createCmd("FS_createDataFile"); // parentPath, filename, data, canRead, canWrite
-    this.createCmd("FS_readFile"); // filename
+    this.createCmd("FS_createDataFile"); // parentPath, filename, data as binary string, canRead, canWrite
+    this.createCmd("FS_readFile"); // filename; returns binary string
     //Private functions
     this.createCmd("FS_createPath"); // parent, name, canRead, canWrite
     this.createCmd("set_TOTAL_MEMORY"); // size
@@ -31,9 +31,18 @@ class TeXLive {
     return binaryString;
   }
 
+  static binaryStringToU8Array(binaryString) {
+    const arrayLength = binaryString.length
+    const byteArray = new Uint8Array(arrayLength);
+    for (let byteId = 0; byteId < arrayLength; byteId++) {
+      byteArray[byteId] = binaryString.charCodeAt(byteId);
+    }
+    return byteArray;
+  }
+
   async compile(src, resourceStrings = [], resourceNames = []) {
     //src is URL of TeX document to run
-    //resourceBuffers is an array of ArrayBuffers of additional input files with corresponding names in resourceNames
+    //resourceBuffers is an array of binary strings of additional input files with corresponding names in resourceNames
     let binary_pdf;
     try {
       try {
@@ -41,7 +50,6 @@ class TeXLive {
         //Upload input files to Emscripten file system
         await this.orError(Promise.all(
           resourceStrings.map(
-            //TODO: Trying with write permissions
             (rString, index) => this.FS_createDataFile("/", resourceNames[index], rString, true, false)
           ).concat(
             this.FS_createDataFile("/", "input.tex", await TeXLive.getFile(src), true, false)
@@ -62,9 +70,8 @@ class TeXLive {
 
     //Return data URL
     if (binary_pdf === false) { throw new Error("PDF failed to compile"); }
-    const outBlob = new Blob([binary_pdf], { type: "application/pdf" });
+    const outBlob = new Blob([TeXLive.binaryStringToU8Array(binary_pdf)], { type: "application/pdf" });
     return URL.createObjectURL(outBlob);
-    // return "data:application/pdf;charset=binary;base64," + btoa(binary_pdf);
   }
 
   terminate() {
@@ -132,31 +139,21 @@ class TeXLive {
     this.worker = new Worker(await workerSrc);
     //Make sure event handler uses this to refer to TeXLive instance
     this.worker.addEventListener("message", (ev) => { this.handleMsg(ev); });
-    this.worker.addEventListener("error", (ev) => {
-      // ev.preventDefault();
-      this.workerOnError("Worker: " + ev.message);
-    });
+    this.worker.addEventListener("error", (ev) => { this.workerOnError("Worker: " + ev.message); });
     await this.orError(workerProm);
 
     //Set memory
     const memSize = 80 * 1024 * 1024;
     const availableSize = await this.orError(this.set_TOTAL_MEMORY(memSize));
-    if (availableSize < memSize) { console.warn("Memory limited to " + availableSize.toString() + "B"); }
+    if (availableSize < memSize) { console.warn("Memory limited to " + availableSize + "B"); }
 
     //Add folders then files to emscripten
     const packages = await packagesProm;
     //Folders are created recursively
     const folderRtns = await this.orError(Promise.all(packages.folders.map((folder) => this.FS_createPath("/", folder, true, true))));
     if (!folderRtns.every((val) => val === true)) { throw new Error("Failed to create folders in Emscripten"); }
-    //TODO: Write permissions only for debugging - change to read only
-    // const tmp = packages.files.map((file) => )
-    // const tmp = await TeXLive.getFile(packages.files[0].fullurl);
-    // await this.orError(Promise.all(packages.files.map(async (file) => this.FS_createDataFile(file.path, file.name, tmp, true, false))));
-
     const fileRtns = await this.orError(Promise.all(packages.files.map(async (file) => this.FS_createDataFile(file.path, file.name, await TeXLive.getFile(file.fullurl), true, false))));
     if (!fileRtns.every((val) => val === true)) { throw new Error("Failed to create files in Emscripten"); }
-
-    // const filesVer = await this.orError(Promise.all(packages.files.map((file) => this.FS_readFile(file.path + "/" + file.name))));
   }
 
   async sendCmd(cmd) {
@@ -190,7 +187,7 @@ TeXLive.getFile = (() => {
       return await fileFetch.text();
       break;
     default:
-      //Assume binary file - convert to binary string
+      //Assume binary file, but safe on all files - convert to binary string
       return TeXLive.arrayBufferToString(await fileFetch.arrayBuffer());
     }
   }
@@ -243,269 +240,3 @@ TeXLive.getPackages = (() => {
     };
   };
 })();
-
-
-// TeXLive.chunksize = (async () => {
-  //   //TODO: Does this need to be async? Is this even needed?
-  //   //Determine the maximum size in bytes of data that can be messaged to worker
-  //   //Uses bisection terminating when size is known to within 200B
-  //   let size = 1024;
-  //   let max = undefined;  //Minimum known failure size
-  //   let min = undefined;  //Maximum known ok size
-  //   let delta = size;
-  //   let success = true;
-  //   let buf;
-  //
-  //   //delta is integer > 50, so can always be represented exactly at double precision - no rounding function required
-  //   while (Math.abs(delta) > 100) {
-    //     if (success) {
-      //       min = size;
-      //       if (max === undefined) {
-        //         delta = size;
-        //       } else {
-          //         delta = 0.5 * (max - size);
-          //       }
-          //     } else {
-            //       max = size;
-            //       if (min === undefined) {
-              //         delta = -0.5 * size;
-              //       } else {
-                //         delta = -0.5 * (size - min);
-                //       }
-                //     }
-                //     size += delta;
-                //
-                //     success = true;
-                //     try {
-                  //       buf = String.fromCharCode.apply(null, new Uint8Array(size));
-                  //       sendCommand({
-                    //         command: "test",
-                    //         data: buf
-                    //       });
-                    //     } catch {
-                      //       success = false;
-                      //     }
-                      //   }
-                      //
-                      //   return size;
-                      // })();
-
-                      // var TeXLive = function(opt_workerPath) {
-                      //   //var self=this;
-                      //   var chunksize= determineChunkSize();
-                      //   if (!opt_workerPath) {
-                      //     opt_workerPath = '';
-                      //   }
-                      //
-                      //
-                      //   var component = function(workerPath) {
-                      //     var self = this;
-                      //     var worker = new Worker(workerPath);
-                      //     self.terminate = function(){worker.terminate()};
-                      //     self.initialized=false;
-                      //     self.on_stdout = function(msg) {
-                      //       console.log(msg);
-                      //     }
-                      //
-                      //     self.on_stderr = function(msg) {
-                      //       console.log(msg);
-                      //     }
-                      //     worker.onmessage = function(ev) {
-                      //       var data = JSON.parse(ev.data);
-                      //       var msg_id;
-                      //       if(!('command' in data))
-                      //         console.log("missing command!", data);
-                      //       switch(data['command']) {
-                      //       case 'ready':
-                      //         onready.done(true);
-                      //         break;
-                      //       case 'stdout':
-                      //       case 'stderr':
-                      //         self['on_'+data['command']](data['contents']);
-                      //         break;
-                      //       default:
-                      //         //console.debug('< received', data);
-                      //         msg_id = data['msg_id'];
-                      //         if(('msg_id' in data) && (msg_id in promises)) {
-                      //           promises[msg_id].done(data['result']);
-                      //         }
-                      //         else
-                      //           console.warn('Unknown worker message '+msg_id+'!');
-                      //       }
-                      //     }
-                      //     var onready = new promise.Promise();
-                      //     var promises = [];
-                      //     var chunkSize = undefined;
-                      //     self.sendCommand = function(cmd) {
-                      //       var p = new promise.Promise();
-                      //       var msg_id = promises.push(p)-1;
-                      //       onready.then(function() {
-                      //         cmd['msg_id'] = msg_id;
-                      //         worker.postMessage(JSON.stringify(cmd));
-                      //       });
-                      //       return p;
-                      //     };
-                      //     self.createCommand = function(command) {
-                      //       self[command] = function() {
-                      //         var args = [].concat.apply([], arguments);
-                      //
-                      //         return self.sendCommand({
-                      //           'command':  command,
-                      //           'arguments': args,
-                      //         });
-                      //       }
-                      //     }
-                      //     self.createCommand('FS_createDataFile'); // parentPath, filename, data, canRead, canWrite
-                      //     self.createCommand('FS_readFile'); // filename
-                      //     self.createCommand('FS_unlink'); // filename
-                      //     self.createCommand('FS_createFolder'); // parent, name, canRead, canWrite
-                      //     self.createCommand('FS_createPath'); // parent, name, canRead, canWrite
-                      //     self.createCommand('FS_createLazyFile'); // parent, name, canRead, canWrite
-                      //     self.createCommand('FS_createLazyFilesFromList'); // parent, list, parent_url, canRead, canWrite
-                      //     self.createCommand('set_TOTAL_MEMORY'); // size
-                      //   };
-                      //
-                      //   var pdftex=new component(opt_workerPath+'pdftex-worker.js');
-                      //   pdftex.compile = function(source_code) {
-                      //     var self=this;
-                      //     var p = new promise.Promise();
-                      //     pdftex.compileRaw(source_code).then(
-                      //       function(binary_pdf) {
-                      //         if(binary_pdf === false)
-                      //           return p.done(false);
-                      //         pdf_dataurl = 'data:application/pdf;charset=binary;base64,' + window.btoa(binary_pdf);
-                      //         return p.done(pdf_dataurl);
-                      //       });
-                      //       return p;
-                      //     };
-                      //     pdftex.compileRaw = function(source_code) {
-                      //       var self=this;
-                      //       return pdftex.run(source_code).then(
-                      //         function() {
-                      //           return self.FS_readFile('/input.pdf');
-                      //         }
-                      //       );
-                      //     };
-                      //     pdftex.run = function(source_code) {
-                      //       var self=this;
-                      //       var commands;
-                      //       if(self.initialized)
-                      //       commands = [
-                      //         curry(self, 'FS_unlink', ['/input.tex']),
-                      //         curry(self, 'FS_createDataFile', ['/', 'input.tex', source_code, true, true])
-                      //       ];
-                      //       else
-                      //       commands = [
-                      //         curry(self, 'FS_createDataFile', ['/', 'input.tex', source_code, true, true]),
-                      //         curry(self, 'FS_createLazyFilesFromList', ['/', 'texlive.lst', './texlive', true, true]),
-                      //       ];
-                      //
-                      //       var sendCompile = function() {
-                      //         self.initialized = true;
-                      //         return self.sendCommand({
-                      //           'command': 'run',
-                      //           'arguments': ['-interaction=nonstopmode', '-output-format', 'pdf', 'input.tex'],
-                      //           //        'arguments': ['-debug-format', '-output-format', 'pdf', '&latex', 'input.tex'],
-                      //         });
-                      //       };
-                      //       return promise.chain(commands)
-                      //       .then(sendCompile)
-                      //     };
-                      //     TeXLive.prototype.pdftex = pdftex;
-                      //
-                      //     var bibtex = new component(opt_workerPath+'bibtex-worker.js');
-                      //     bibtex.compile = function(aux){
-                      //       var self=this;
-                      //       var p = new promise.Promise();
-                      //       bibtex.compileRaw(aux).then(
-                      //         function(binary_bbl) {
-                      //           if(binary_bbl === false)
-                      //             return p.done(false);
-                      //           pdf_dataurl = 'data:text/plain;charset=binary;base64,' + window.btoa(binary_bbl);
-                      //           return p.done(pdf_dataurl);
-                      //         });
-                      //         return p;
-                      //       };
-                      //       bibtex.compileRaw = function(aux) {
-                      //         var self=this;
-                      //         return bibtex.run(aux).then(
-                      //           function() {
-                      //             return self.FS_readFile('/input.bbl');
-                      //           }
-                      //         );
-                      //       };
-                      //       bibtex.run = function(source_code) {
-                      //         var self=this;
-                      //         var commands;
-                      //         if(self.initialized)
-                      //         commands = [
-                      //           curry(self, 'FS_unlink', ['/input.aux']),
-                      //           curry(self, 'FS_createDataFile', ['/', 'input.aux', aux, true, true])
-                      //         ];
-                      //         else
-                      //         commands = [
-                      //           curry(self, 'FS_createDataFile', ['/', 'input.aux', aux, true, true]),
-                      //           curry(self, 'FS_createLazyFilesFromList', ['/', 'texlive.lst', './texlive', true, true]),
-                      //         ];
-                      //         var sendCompile = function() {
-                      //           self.initialized = true;
-                      //           return self.sendCommand({
-                      //             'command': 'run',
-                      //             'arguments': ['input.aux'],
-                      //           });
-                      //         };
-                      //         return promise.chain(commands)
-                      //         .then(sendCompile)
-                      //       };
-                      //       TeXLive.prototype.bibtex=bibtex;
-                      //       TeXLive.prototype.terminate = function(){
-                      //         pdftex.terminate();
-                      //         bibtex.terminate();
-                      //       }
-                      //     };
-                      //     var determineChunkSize = function() {
-                      //       var size = 1024;
-                      //       var max = undefined;
-                      //       var min = undefined;
-                      //       var delta = size;
-                      //       var success = true;
-                      //       var buf;
-                      //
-                      //       while(Math.abs(delta) > 100) {
-                      //         if(success) {
-                      //           min = size;
-                      //           if(typeof(max) === 'undefined')
-                      //             delta = size;
-                      //           else
-                      //             delta = (max-size)/2;
-                      //         }
-                      //         else {
-                      //           max = size;
-                      //           if(typeof(min) === 'undefined')
-                      //             delta = -1*size/2;
-                      //           else
-                      //             delta = -1*(size-min)/2;
-                      //         }
-                      //         size += delta;
-                      //
-                      //         success = true;
-                      //         try {
-                      //           buf = String.fromCharCode.apply(null, new Uint8Array(size));
-                      //           sendCommand({
-                      //             command: 'test',
-                      //             data: buf,
-                      //           });
-                      //         }
-                      //         catch(e) {
-                      //           success = false;
-                      //         }
-                      //       }
-                      //
-                      //       return size;
-                      //     };
-                      //
-                      //     curry = function(obj, fn, args) {
-                      //       return function() {
-                      //         return obj[fn].apply(obj, args);
-                      //       }
-                      //     }
